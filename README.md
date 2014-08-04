a4861e71d5e44334f3a83b0db0beb07abac37919
hiwayche <hiwayche@gmail.com>
2014-08-04 18:52:39 +0800

    first edition

---
+++ README.md
@@ -0,0 +1,2 @@
+Press ctrl + i to make the first input element in page to gain focus
+This is very convenient when you search with google.
---
+++ focus.js
@@ -0,0 +1,31 @@
+function getFirstFocusableInput(){
+	var inputArray = Array.prototype.slice.call(document.getElementsByTagName("input"));
+	if(!inputArray){
+		return;
+	}
+	if(!inputArray.push){
+		return inputArray;
+	}
+	var input = undefined;
+	while(inputArray.length > 0){
+		input = inputArray.shift();
+		if(input.getAttribute("type") == 'text' && input.getAttribute("readonly") == undefined && input.getAttribute("disabled") == undefined){
+			break;
+		}else{
+			input = undefined;
+		}
+	}
+	return input;
+}
+
+function keyDown(event){
+	if(event.keyCode == 73 && event.ctrlKey){
+		var input = getFirstFocusableInput();
+		if(input){
+			console.log(input);
+			input.focus();
+		}
+	}
+}
+
+document.addEventListener("keydown", keyDown, false);
